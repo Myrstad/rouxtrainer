a0831507c3aef583d62477731e6c8e78b2b32516
Myrstad <mortenam@uia.no>
2025-07-23 19:15:52 +0200

    💄 Card styling and functionality

--- frontend/src/components/CMLL/Trainer.svelte
+++ frontend/src/components/CMLL/Trainer.svelte
@@ -16,6 +16,7 @@
 
     let reversedScrambleDisplay = "";
     let showSolution = false;
+    let lastOutcome: 'fail' | 'unsure' | null = null;
 
     const trainerCube = new Cube(3);
     let renderer3DInstance: Renderer3D | undefined = undefined;
@@ -53,22 +54,26 @@
     function handleSuccess() {
         if (!currentCase) return;
         cmllTrainerStore.recordAttempt(currentCase.id, 'success');
+        lastOutcome = null;
         currentCaseIndex++; // Move to the next case
     }
 
     function handleUnsure() {
         if (!currentCase) return;
         cmllTrainerStore.recordAttempt(currentCase.id, 'unsure');
+        lastOutcome = 'unsure';
         showSolution = true; // Reveal the solution
     }
 
     function handleFail() {
         if (!currentCase) return;
         cmllTrainerStore.recordAttempt(currentCase.id, 'fail');
+        lastOutcome = 'fail';
         showSolution = true; // Reveal the solution
     }
 
     function handleNextAfterReveal() {
+        lastOutcome = null;
         currentCaseIndex++; // Move to the next case
     }
 
@@ -80,6 +85,7 @@
         trainerCube.paintRoux();
 
         showSolution = false;
+        lastOutcome = null;
 
         const algorithmMoves = trainerCube.getMovesFromAlgorithm(currentCase.preferredAlgorithm);
         const reversedMoves = trainerCube.reverseMoves(algorithmMoves);
@@ -122,80 +128,292 @@
         }
     }
 </script>
-<div class="card">
-    <div>
-        <button on:click={()=>navigateTo('home')}>Go back</button> Trainer
+<div class="trainer">
+    <div class="tab-navigation">
+        <button on:click={()=>navigateTo('home')}>&lt;-- Go back</button> <h1>Trainer</h1>
     </div>
     {#if sessionElements.length === 0}
         <p>No cases selected for practice. Please check your settings or try again later.</p>
-        <button on:click={() => navigateTo('home')}>Go to Home</button>
+        <button on:click={() => navigateTo('settings')}>Go to Settings</button>
     {:else if currentCase}
-        <div class="case-info">
-            <p>Case: {currentCase.id} ({currentCaseIndex +1 }/{sessionElements.length})</p>
-            <p>Setup Scramble: {reversedScrambleDisplay}</p>
-        </div>
-
-        <div class="view-toggle">
-            <button on:click={() => setView('3D')} class:active={currentView === '3D'}>3D View</button>
-            <button on:click={() => setView('2D')} class:active={currentView === '2D'}>2D View</button>
-        </div>
-
-        <div class="cube-display-container">
-            <canvas bind:this={canvasElement3D} width="600" height="600" class="cube-view" style="display: {currentView === '3D' ? 'block' : 'none'};"></canvas>
-            <div bind:this={previewContainerElement2D} class="preview-2d" style="display: {currentView === '2D' ? 'block' : 'none'};"></div>
-        </div>
-
-        {#if !showSolution}
-            <div class="action-buttons">
-                <button on:click={handleSuccess} class="success">Success</button>
-                <button on:click={handleUnsure} class="unsure">Unsure</button>
-                <button on:click={handleFail} class="fail">Fail</button>
+        <div class="card">
+            <div class="view-toggle">
+                <button on:click={() => setView('3D')} class:active={currentView === '3D'}>3D</button>
+                <button on:click={() => setView('2D')} class:active={currentView === '2D'}>2D</button>
             </div>
-        {:else}
-            <div class="solution-revealed">
-                <p>Solution Algorithm: {currentCase.preferredAlgorithm}</p>
-                <button on:click={handleNextAfterReveal}>Okay / Next</button>
+    
+            <div class="cube-display-container">
+                <canvas bind:this={canvasElement3D}  class="cube-view" style="display: {currentView === '3D' ? 'block' : 'none'};"></canvas>
+                <div bind:this={previewContainerElement2D} class="preview-2d" style="display: {currentView === '2D' ? 'block' : 'none'};"></div>
             </div>
-        {/if}
+    
+            
+            {#if !showSolution}
+                <div class="case-info">
+                    <p class="group">
+                        {cmllTrainerStore.getCaseDefinition(currentCase.id)?.group}
+                    </p>
+                    <div class="column">
+                        <span class="number">
+                            {"0"+currentCase.id.replace(/\D/g, '')}
+                        </span>
+                        <span class="name">
+                            {cmllTrainerStore.getCaseDefinition(currentCase.id)?.name}
+                        </span>
+                    </div>
+                </div>
+                <p title="Setup" class="setup">
+                    {reversedScrambleDisplay}
+                </p>
+                <div class="action-buttons">
+                    <button on:click={handleUnsure} class="unsure">Unsure</button>
+                    <button on:click={handleSuccess} class="success">Success</button>
+                    <button on:click={handleFail} class="fail">Fail</button>
+                </div>
+            {:else}
+                <div class="solution-container">
+
+                    {#if lastOutcome === 'fail'}
+                        <div class="chip fail">Failed</div>
+                    {:else if lastOutcome === 'unsure'}
+                        <div class="chip unsure">Unsure / Did not try</div>
+                    {/if}
+                    <p class="name">
+                        {cmllTrainerStore.getCaseDefinition(currentCase.id)?.group+"0"+currentCase.id.replace(/\D/g, '')}
+                        {cmllTrainerStore.getCaseDefinition(currentCase.id)?.name}
+                    </p>
+                    <p class="solution" title="solution">{currentCase.preferredAlgorithm}</p>
+                </div>
+                <div class="action-buttons">
+                    <button class="next" on:click={handleNextAfterReveal}>Okay</button>
+                </div>
+            {/if}
+        </div>
+        <div class="session-progress">
+            {#each { length: sessionElements.length } as _, i}
+                <div class="progress-dot" class:active={i === currentCaseIndex}></div>
+            {/each}
+        </div>
     {:else}
         <p>Session finnished</p>
     {/if}
 </div>
 
 <style>
+    .tab-navigation {
+        display: flex;
+        gap: 1rem;
+        align-items: center;
+        margin-bottom: 2rem;
+    }
+
+    .tab-navigation button {
+        font-size: 1rem;
+        line-height: 100%;
+        padding: 0.5rem 1.25rem;
+        border-radius: 2rem;
+        border: 1px solid var(--neutral-900);
+        color: var(--neutral-900);
+        background-color: var(--neutral-100);
+    }
+
+    .tab-navigation h1 {
+        margin-top: 0;
+        color: var(--neutral-900);
+        font-size: 1.25rem;
+        font-weight: 400;
+        margin-bottom: 0;
+    }
+
     button {
         cursor: pointer;
     }
 
     .card {
+        margin: 0 auto;
         display: flex;
         flex-direction: column;
+        align-items: center;
+        background-color: var(--neutral-100);
+        border: 2px solid var(--neutral-900);
+        border-radius: 1rem;
+        padding: 1rem;
+        max-width: 400px;
+        color: var(--neutral-900);
+        gap: 0.75rem;
     }
 
     canvas {
         user-select: none;
-        width: 600px;
-        height: 600px;
+        width: 100%;
+        aspect-ratio: 1/1;
     }
 
     .cube-display-container {
+        width: 100%;
         display: flex;
         justify-content: center;
         align-items: center;
-        min-height: 600px; /* Adjust as needed */
+        background-color: var(--neutral-0);
+        border-radius: 2rem;
     }
 
     .cube-display-container .preview-2d {
-        width: 400px; /* Adjust size for 2D preview */
-        height: 400px;
-        border: 1px solid #ccc; /* Optional: for visual debugging */
+        width: 100%;
+        aspect-ratio: 1/1;
+        margin: 3rem;
     }
 
+    .case-info {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
 
+    .case-info .group {
+        font-size: 2.5rem;
+        color: inherit;
+        margin: 0;
+    }
+
+    .case-info .number {
+        font-size: 0.75rem;
+        line-height: 100%;
+        color: #2F2FFF;
+    }
+    
+    .case-info .name {
+        font-size: 1.25rem;
+        line-height: 100%;
+        color: var(--neutral-1000);
+    }
+
+    .case-info .column {
+        display: flex;
+        flex-direction: column;
+    }
+
+    p.setup {
+        margin: 0;
+        margin-top: 0.75rem;
+        text-align: center;
+        font-weight: 500;
+    }
 
     .view-toggle {
-        margin-bottom: 1rem;
         display: flex;
-        gap: 10px;
+        gap: 0.25rem;
+        padding: 0.25rem;
+        border-radius: 10rem;
+        background-color: var(--neutral-200);
+        outline: 1px solid var(--neutral-0);
+        outline-offset: -1px;
     }
+
+    .view-toggle button {
+        outline: none;
+        border: none;
+        color: var(--neutral-900);
+        background-color: var(--neutral-100);
+        border-radius: 10rem;
+        font-size: 0.75rem;
+        padding: 0.125rem 0.5rem;
+    }
+
+    .view-toggle button.active {
+        background-color: var(--neutral-0);
+        font-weight: 500;
+    }
+
+    .action-buttons {
+        display: flex;
+        gap: 0.5rem;
+        width: 100%;
+    }
+
+    .action-buttons button {
+        outline: none;
+        border: none;
+        font-weight: 500;
+        border-radius: 0.25rem;
+        font-size: 0.875rem;
+        background-color: var(--neutral-200);
+        height: 2rem;
+        padding: 0 0.875rem;
+        text-transform: uppercase;
+    }
+
+    .action-buttons button.success,
+    .action-buttons button.next {
+        flex: 1;
+        border-radius: 10rem;
+        background-color: var(--green-300);
+    }
+    .action-buttons button.fail {
+        background-color: var(--red-500);
+    }
+    .action-buttons button.unsure {
+        background-color: var(--yellow-300);
+    }
+
+    .solution-container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 0.5rem;
+    }
+
+    .solution-container p {
+        margin: 0;
+    }
+
+    .solution-container .name {
+        font-size: 1.25rem;
+        font-weight: 300;
+        line-height: 100%;
+    }
+
+    .solution-container .solution {
+        color: var(--neutral-1000);
+        font-weight: 500;
+    }
+
+    .chip {
+        color: var(--neutral-900);
+        border-radius: 0.25rem;
+        padding: 0.125 0.5rem;
+        text-transform: uppercase;
+    }
+
+    .chip.unsure {
+        background-color: var(--yellow-300);
+    }
+    .chip.fail {
+        background-color: var(--red-500);
+    }
+
+    .session-progress {
+        margin: 0 auto;
+        margin-top: 1rem;
+        display: flex;
+        width: fit-content;
+        gap: 0.125rem;
+        padding: 0.125rem;
+        background-color: var(--neutral-100);
+        border-radius: 10rem;
+        border: 1px solid var(--neutral-900);
+    }
+
+    .session-progress div {
+        display: block;
+        width: 0.75rem;
+        height: 0.75rem;
+        border-radius: 10rem;
+        background-color: var(--neutral-200);
+    }
+
+    .session-progress .active {
+        background-color: var(--neutral-0);
+    }
+
 </style>
